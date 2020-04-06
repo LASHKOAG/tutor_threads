372a802c261e63fc7b668793834d3dc382e18961
lashkoag <lashko-ag@energos.su>
2020-04-06 14:45:33 +0600

    thread with parameters

--- simpleCode_threads1/main.cpp
+++ simpleCode_threads1/main.cpp
@@ -8,23 +8,27 @@ using namespace std;
 //Многопоточное программирование
 //передача параметров в поток
 
-void DoWork(){
-    for(size_t i = 0; i < 10; ++i){
-        cout << "ID of thread = " << this_thread::get_id() << "\tDoWork " << i << endl;
-        this_thread::sleep_for(chrono::milliseconds(1000));
-    }
+void DoWork(int a, int b){
+    this_thread::sleep_for(chrono::milliseconds(3000));
+        cout << "=============\t" << "DoWork STARTED\t=================" << endl;
+    this_thread::sleep_for(chrono::milliseconds(5000));
+        cout << "a+b = " << a+b << endl;
+    this_thread::sleep_for(chrono::milliseconds(3000));
+        cout << "=============\t" << "DoWork STOPPED\t=================" << endl;
 }
 
 
 int main()
 {
-    thread th(DoWork);
+    thread th(DoWork,2,3);
 
     //th.detach();
     //th.join();
+    //DoWork(2,3);
 
-    for(size_t i = 0; i < 10; ++i){
-        cout << "ID of thread = " << this_thread::get_id() << "\tmain " << i << endl;
+
+    for(size_t i = 0; true; ++i){
+        cout << "ID of thread = " << this_thread::get_id() << "\tmain works\t" << i << endl;
         this_thread::sleep_for(chrono::milliseconds(500));
     }
 
