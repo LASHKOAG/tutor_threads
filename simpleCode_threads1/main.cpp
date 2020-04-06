0d2f15ac0f0393204bf22f5d5d2329496cd70bb1
lashkoag <lashko-ag@energos.su>
2020-04-06 13:30:14 +0600

    for cout id of thread, chrono(milliseconds)

--- simpleCode_threads1/main.cpp
+++ simpleCode_threads1/main.cpp
@@ -12,13 +12,19 @@ using namespace std;
 
 int main()
 {
-    cout << "START MAIN" << endl;
+    for(size_t i = 0; i < 10; ++i){
+        cout << "ID of thread = " << this_thread::get_id() << "\tmain" << endl;
+        this_thread::sleep_for(chrono::milliseconds(3000));
+    }
 
-    cout << this_thread::get_id() << endl;
-
-    this_thread::sleep_for(chrono::milliseconds(1000));
-
-    cout << "END MAIN" << endl;
 
     return 0;
 }
+
+//cout << "START MAIN" << endl;
+
+//cout << this_thread::get_id() << endl;
+
+//this_thread::sleep_for(chrono::milliseconds(1000));
+
+//cout << "END MAIN" << endl;
