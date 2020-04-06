c145d9889b3ec971c6ae3b5af921342486c6bd82
lashkoag <lashko-ag@energos.su>
2020-04-06 13:48:02 +0600

    thread.detach()

--- simpleCode_threads1/main.cpp
+++ simpleCode_threads1/main.cpp
@@ -9,12 +9,23 @@ using namespace std;
 //потоки
 //thread
 
+void DoWork(){
+    for(size_t i = 0; i < 10; ++i){
+        cout << "ID of thread = " << this_thread::get_id() << "\tDoWork " << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+}
+
 
 int main()
 {
+    thread th(DoWork);
+
+    th.detach();
+
     for(size_t i = 0; i < 10; ++i){
-        cout << "ID of thread = " << this_thread::get_id() << "\tmain" << endl;
-        this_thread::sleep_for(chrono::milliseconds(3000));
+        cout << "ID of thread = " << this_thread::get_id() << "\tmain " << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
     }
 
 
