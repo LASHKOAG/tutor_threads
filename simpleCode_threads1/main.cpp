86f8c4f6c06b311355ab4271123dea24486bb6e5
lashkoag <lashko-ag@energos.su>
2020-04-06 13:21:10 +0600

    first step sleep_for, get_id

---
+++ simpleCode_threads1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+//урок 1
+//Многопоточное программирование
+//потоки
+//thread
+
+int main()
+{
+    cout << "START MAIN" << endl;
+
+    cout << this_thread::get_id() << endl;
+
+    this_thread::sleep_for(chrono::milliseconds(1000));
+
+    cout << "END MAIN" << endl;
+
+    return 0;
+}
